b528a5c1810224597113a6b66a352c961917267a
Jev22 <jesperlhn@gmail.com>
2021-02-19 10:41:26 +0100

    Added class Child Added createChild Added List to hold new Child

--- src/RoskildeFrieBornehave.java
+++ src/RoskildeFrieBornehave.java
@@ -1,8 +1,42 @@
+
+import java.util.*;
+
+
 public class RoskildeFrieBornehave {
 
     public static void main (String[] args ) {
-        System.out.println("hej");
-        System.out.println("Update1");
+        Scanner input = new Scanner(System.in);
+        List<Child> childList = new LinkedList<>();
+
+        childList.add(createChild(input));
+        System.out.println(childList);
+
+    }
+
+    private static Child createChild(Scanner input) {
+        System.out.println("Enter first name: ");
+        String firstName = input.next();
+        System.out.println("Enter last name: ");
+        String lastName = input.next();
+        System.out.println("Enter Cpr nr. format: DDMMYYYY: ");
+        int cprNr = input.nextInt();
+        System.out.println("Start date: ");
+        int startDate = input.nextInt();
+        System.out.println(" Stue: Yellow, red, green");
+        String room = input.next();
+        System.out.println("Parent id: ");
+        int parentID = input.nextInt();
+        System.out.println("Current date: ");
+        int date = input.nextInt();
+        System.out.println("Add child to waitlist? y/n");
+        String waitListAnswer = input.next();
+        boolean waitList = false;
+        if(waitListAnswer.equalsIgnoreCase("y")){
+            waitList = true;
+        }
+        Child newChild = new Child(firstName, lastName, cprNr, startDate, room, parentID, date, waitList);
+        return newChild;
+
     }
 
 }
